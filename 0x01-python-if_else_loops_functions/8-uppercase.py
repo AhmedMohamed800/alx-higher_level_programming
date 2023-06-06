741625636443c938f75f4f757c3ae6249f5482e8
Ahmed Mohamed <=>
2023-06-06 11:56:11 +0000

    testo

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,6 @@ def uppercase(str):
     for char in str:
         if ord(char) in range(97, 123):
             upper = chr(ord(char) - 32)
-            print("{}".format(upper), end="")
+            print("{}".format(upper), end="" if str[-1] != char else "\n")
         else:
             print("{}".format(char), end="" if str[-1] != char else "\n")
