f994cd38f9051cef370bc142e657d02e993882c4
Ahmed Mohamed <=>
2023-06-06 09:06:14 +0000

    fix 3 and 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,4 @@
 for i in range(10):
     for y in range(1, 10):
         if y != i and y > i:
-            print(f"{i}{y}", end=", " if i != 8 or y != 9 else "\n")
+            print("{0}{1}".format(i, y), end=", " if i != 8 or y != 9 else "\n")
