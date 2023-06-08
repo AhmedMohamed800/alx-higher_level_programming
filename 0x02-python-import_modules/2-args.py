555cb44f8bd9c5c05fe865ae87a94a376a7f0e20
Ahmed Mohamed <=>
2023-06-08 06:45:49 +0000

    0x02-python-import_modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ != "__main__":
+    exit()
+
+arguments = sys.argv
+arg_len = len(arguments)
+message = "argument"
+
+if arg_len == 1:
+    message += "s."
+elif arg_len == 2:
+    message += ":"
+elif arg_len > 2:
+    message += "s:"
+print("{:d} {:s}".format(arg_len - 1 ,message))
+for num in range(1, arg_len):
+    print("{:d}: {:s}".format(num, arguments[num]))
