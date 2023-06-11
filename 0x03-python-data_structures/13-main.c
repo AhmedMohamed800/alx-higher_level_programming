ddda69be030e20feb20c72a7d5c1dea961a34a0b
Ahmed Mohamed <=>
2023-06-11 06:16:56 +0000

    deal with 1 element in is_palindrome

--- 0x03-python-data_structures/13-main.c
+++ 0x03-python-data_structures/13-main.c
@@ -12,6 +12,10 @@ int main(void)
     listint_t *head;
 
     head = NULL;
+    add_nodeint_end(&head, 1);
+    add_nodeint_end(&head, 0);
+    add_nodeint_end(&head, 0);
+    add_nodeint_end(&head, 1);
     print_listint(head);
 
     if (is_palindrome(&head) == 1)
