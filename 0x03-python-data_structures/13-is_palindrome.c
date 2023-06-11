376dda30813601d3024181905063ebb871cda080
Ahmed Mohamed <=>
2023-06-11 06:10:18 +0000

    fix more problems with palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -9,9 +9,9 @@
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *current = *head;
+	listint_t *current = *head, *second = *head;
 	int size_of_list = 1;
-	int *numys, i = 0, j = 0;
+	int *numys, i = 0, j = 0, t = 0;
 
 	if (current == NULL || current->next == NULL)
 		return (1);
@@ -23,11 +23,18 @@ int is_palindrome(listint_t **head)
 	if (size_of_list % 2 == 0)
 	{
 		numys = malloc(size_of_list * sizeof(int));
+		while (second != NULL)
+		{
+			numys[t] = second->n;
+			second = second->next;
+			t++;
+		}
 		j = size_of_list - 1;
 		for (i = 0; i < size_of_list / 2; i++)
 		{
 			if (numys[i] != numys[j])
 				return (0);
+			j--;
 		}
 		free(numys);
 		return (1);
