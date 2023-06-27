8c62b5cbeb3ecd38f0fda02e2da7b3c4c3811013
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2023-06-27 15:26:44 +0000

    Add 2-squqre.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" The file is a continue of the previous task"""
+
+
+class Square:
+    """defines a square by based on 1-square.py"""
+    def __init__(self, size=0):
+        """
+        defines the variables of Square
+
+        Args:
+            self: the self argument of the class
+            size: square's size
+
+        Returns:
+            noting
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = int(size)
