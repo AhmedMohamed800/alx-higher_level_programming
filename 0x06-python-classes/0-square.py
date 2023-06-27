f5934d1d44a7674a3bdf1bd2751bd0a44863c361
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2023-06-27 10:50:35 +0000

    Test

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,15 +1,3 @@
 #!/usr/bin/python3
 class Square:
-    """
-    Empty class that defines a square
-    
-    Attributes
-    ----------
-    None
-
-    Methods
-    -------
-    None
-    """
-
     pass
