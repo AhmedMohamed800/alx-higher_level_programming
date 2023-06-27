ed572c9dea7016141a6762fe4e4daecca49c41a7
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2023-06-27 15:06:16 +0000

    Add 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" This file aims to manipulate square's size"""
+
+
+class Square:
+    """Square that defines a square by"""
+    def __init__(self, size):
+        """
+        Defines the vaiarbles of a class
+
+        Args:
+            self: the self argument
+            size: size of a square
+
+        Returns:
+            Noting
+        """
+        self.__size = size
