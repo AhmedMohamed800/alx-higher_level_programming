6695ceff4df0fdea7c369de78009cf2bc583d726
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2023-07-03 10:57:48 +0000

    Handle 2 test cases with 9-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -81,6 +81,10 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
-        width = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         height = size
+        width = size
         return cls(width, height)
