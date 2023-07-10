2daf5e03a8ea97a38182b16eca243f0b6f97ea7b
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2023-07-10 12:04:10 +0000

    Add 10-square.py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+"""Module of BaseGeometry"""
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.size = size
