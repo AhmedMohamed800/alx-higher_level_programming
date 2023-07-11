c7e1ccdd5267b9bd0db88cf5aac7b29329e29fdd
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2023-07-11 15:13:54 +0000

    Add 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Student Class"""
+
+
+class Student:
+    """defines a student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attr=None):
+        if attr is None:
+            return self.__dict__
+        dicty = {}
+        for ele in attr:
+            if ele in self.__dict__.keys():
+                dicty[ele] = self.__dict__[ele]
+        return dicty
+
+    def reload_from_json(self, json):
+        self.first_name = json["first_name"]
+        self.last_name = json["last_name"]
+        self.age = json["age"]
