f24a4263fc4c5acb84b33f8b304b8f926cdcbf05
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2023-07-14 12:21:40 +0000

    Add files

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -11,16 +11,78 @@ class Rectangle(Base):
             raise TypeError("width must be an integer")
         elif width <= 0:
             raise ValueError("width must be > 0")
-        self.width = width
+        self.__width = width
         if type(height) is not int:
             raise TypeError("height must be an integer")
         elif height <= 0:
             raise ValueError("height must be > 0")
-        self.height = height
-        if x < 0:
+        self.__height = height
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
+        elif x < 0:
             raise ValueError("x must be >= 0")
-        self.x = x
-        if y < 0:
+        self.__x = x
+        if type(y) is not int:
+            raise TypeError("y must be an integer")
+        elif y < 0:
             raise ValueError("y must be >= 0")
-        self.y = y
+        self.__y = y
         super().__init__(id)
+
+    @property
+    def width(self):
+        """access width value"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """change width value"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """access height value"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """change width value"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """access x value"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """access y value"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """return area of Rect"""
+        return self.__width * self.__height
