a27b20bfa4c2e79f23daf3ef36c4d0aefd8b7222
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2023-07-15 10:44:58 +0000

    Add to json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,7 @@
 Module of base
 ==============
 """
+import json
 
 
 class Base:
@@ -22,3 +23,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """to json"""
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        return json.dumps(list_dictionaries)
