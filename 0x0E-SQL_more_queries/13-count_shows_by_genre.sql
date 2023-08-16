d7f43332a4f7b3f5e170b87e4a5c4e7af7eed062
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2023-08-16 17:38:04 +0000

    fix

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- lists all shows contained in the database hbtn_0d_tvshows.
-SELECT tv_genres.name, COUNT(*) AS number_of_shows FROM tv_genres
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows FROM tv_genres
 JOIN tv_show_genres
 ON tv_show_genres.genre_id=tv_genres.id
 GROUP BY tv_show_genres.genre_id
