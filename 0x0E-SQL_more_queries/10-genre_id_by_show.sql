0c7e913a0d5790422520c1e57db70b67b2d29435
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2023-08-16 13:11:20 +0000

    t

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
 -- import the database dump from hbtn_0d_tvshows
-SELECT t.title, g.genre_id FROM tv_shows AS t
-INNER JOIN tv_show_genres AS g ON g.genre_id=t.id
-ORDER BY t.title, g.genre_id;
+SELECT t.`title`, g.`genre_id` FROM `tv_shows` AS t
+INNER JOIN `tv_show_genres` AS g ON g.`show_id`=t.`id`
+ORDER BY t.`title`, g.`genre_id`;
