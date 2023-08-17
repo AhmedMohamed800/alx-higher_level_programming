8dc9f0bd6c7ee33f5addf022db2a2f3fe465e565
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2023-08-17 03:38:47 +0000

    fix 16

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -2,4 +2,4 @@
 SELECT t.title, g.name FROM tv_shows AS t
 LEFT JOIN tv_show_genres AS d ON t.id=d.show_id
 LEFT JOIN tv_genres AS g ON g.id=d.genre_id
-ORDER BY t.title, g.name DESC;
+ORDER BY t.title, g.name;
