df7f6700c3fdca3ffa1310c95f0602e46cc3d017
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2023-08-17 03:37:50 +0000

    Add 16

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,5 @@
+-- shows, and all genres linked to that show
+SELECT t.title, g.name FROM tv_shows AS t
+LEFT JOIN tv_show_genres AS d ON t.id=d.show_id
+LEFT JOIN tv_genres AS g ON g.id=d.genre_id
+ORDER BY t.title, g.name DESC;
