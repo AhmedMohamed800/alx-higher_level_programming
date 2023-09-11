734ccc725ca56678219cb5f16cd68a4a11e01e74
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2023-09-11 14:27:20 +0000

    Capital x in  8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,7 +4,7 @@ const args = process.argv;
 
 if (args[2] && parseInt(args[2])) {
   for (let i = 0; i < parseInt(args[2]); i++) {
-    console.log('x'.repeat(parseInt(args[2])));
+    console.log('X'.repeat(parseInt(args[2])));
   }
 } else {
   console.log('Missing size');
