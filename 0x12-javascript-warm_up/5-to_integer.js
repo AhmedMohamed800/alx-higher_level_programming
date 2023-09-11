aa6e0cc2f206bb719a6748565a933fed6bc76b68
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2023-09-11 13:57:36 +0000

    remove is from the

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,7 +4,7 @@ const args = process.argv;
 
 if (args[2]) {
   if (parseInt(args[2])) {
-    console.log(`My number is: ${parseInt(args[2])}`);
+    console.log(`My number: ${parseInt(args[2])}`);
   } else {
     console.log('Not a number');
   }
