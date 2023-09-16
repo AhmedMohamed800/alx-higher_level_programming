a5ab340fce3d84cfc116aab2a80e551040266e42
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2023-09-16 10:19:11 +0000

    fix 11

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -15,4 +15,5 @@ if __name__ == "__main__":
     new_state = State(name="Louisiana")
     session.add(new_state)
     session.commit()
+    print(session.query(State).filter(State.name == "Louisiana").first().id)
     session.close()
