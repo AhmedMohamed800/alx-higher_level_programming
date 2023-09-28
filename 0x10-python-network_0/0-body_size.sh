64196650ced7078047c8afa4ac74fd00f5fb571f
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2023-09-28 12:10:58 +0000

    change file lines

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a request to that UR
-url=$1
-result=$(curl -Is "$url" | grep -i Content-Length | awk '{print $2}')
-echo "$result"
+curl -Is "$1" | grep -i Content-Length | awk '{print $2}'
