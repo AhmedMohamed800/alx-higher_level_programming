67a190dbb992c01a0a81598761c9f08a81d00576
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2023-10-23 08:53:03 +0000

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,25 +3,27 @@
 import sys
 import requests
 
-EMPLOYID = int(sys.argv[1])
-TODOURL = f"https://jsonplaceholder.typicode.com/todos?userId={EMPLOYID}"
-USER = f"https://jsonplaceholder.typicode.com/users/{EMPLOYID}"
-response = requests.get(USER)
 
-if response.status_code == 200:
-    DATA = response.json()
-    TASKS = requests.get(TODOURL).json()
-    com = 0
-    unfinished = 0
-    for task in TASKS:
-        if task['completed']:
-            com += 1
-        else:
-            unfinished += 1
-    totale = com + unfinished
-    print(f"Employee {DATA['name']} is done with tasks ({com}/{totale}):")
-    for task in TASKS:
-        if task['completed']:
-            print(f"\t {task['title']}")
-else:
-    print(f"Request failed with status code: {response.status_code}")
+if __name__ == "__main__":
+    EMPLOYID = int(sys.argv[1])
+    TODOURL = f"https://jsonplaceholder.typicode.com/todos?userId={EMPLOYID}"
+    USER = f"https://jsonplaceholder.typicode.com/users/{EMPLOYID}"
+    response = requests.get(USER)
+
+    if response.status_code == 200:
+        DATA = response.json()
+        TASKS = requests.get(TODOURL).json()
+        com = 0
+        unfinished = 0
+        for task in TASKS:
+            if task.get('completed'):
+                com += 1
+            else:
+                unfinished += 1
+        to = com + unfinished
+        print(f"Employee {DATA.get('name')} is done with tasks ({com}/{to}):")
+        for task in TASKS:
+            if task.get('completed'):
+                print(f"\t {task.get('title')}")
+    else:
+        print(f"Request failed with status code: {response.status_code}")
