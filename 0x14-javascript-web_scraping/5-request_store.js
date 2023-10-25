4d23cc153b9ffdb1610f489a9701473c6aa5d7eb
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2023-10-25 07:18:02 +0000

    fix semi

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -9,9 +9,8 @@ request(URL, (error, response, body) => {
   if (error) {
     console.error(error);
   } else {
-    const RESULT = body;
     fs.writeFile(FILENAME, body, 'utf8', (err) => {
-	    if (err) console.error(err);
-    })
+      if (err) console.error(err);
+    });
   }
 });
