003706e05602a4075ef0b31e48ccd25f8f6e6d92
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2023-10-25 07:15:25 +0000

    Add  6-completed_tasks.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,21 +1,17 @@
 #!/usr/bin/node
 
 const request = require('request');
+const fs = require('fs');
 const URL = process.argv[2];
+const FILENAME = process.argv[3];
 
 request(URL, (error, response, body) => {
   if (error) {
     console.error(error);
   } else {
-    const TODOS = {};
-    const RESULTS = JSON.parse(body);
-    RESULTS.map((task) => {
-      if (!Object.prototype.hasOwnProperty.call(TODOS, '' + task.userId)) {
-        TODOS['' + task.userId] = 0;
-      }
-      if (task.completed) TODOS['' + task.userId] += 1;
-      return task;
-    });
-    console.log(TODOS);
+    const RESULT = body;
+    fs.writeFile(FILENAME, body, 'utf8', (err) => {
+	    if (err) console.error(err);
+    })
   }
 });
